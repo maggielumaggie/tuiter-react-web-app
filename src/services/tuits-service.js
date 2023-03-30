14db44d3152fa0c314500452d5646699096fcc20
maggielumaggie <mqlu@outlook.com>
2023-03-30 15:11:30 -0700

    set bash

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // const TUITS_API = 'https://tuiter-node-server-app-0q2i.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
+console.log(API_BASE);
 
 export const findTuits = async () => {
   const response = await axios.get(TUITS_API);
